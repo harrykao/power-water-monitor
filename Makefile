72c27f5359449bbdc8561664df24158d8128ee1c
Harry Kao <harry@hairycow.name>
2021-06-19 09:33:00 -0700

    Authenticate against Rainforest API.
    Set credentials via environment variables.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build:
 	docker build -t energy_water_monitor .
 
 run: build
-	docker run -it --rm energy_water_monitor
+	docker run -it --rm --env-file=env energy_water_monitor
 
 shell: build
-	docker run -it --rm --entrypoint /bin/sh energy_water_monitor
+	docker run -it --rm --env-file=env --entrypoint /bin/sh energy_water_monitor
--- src/app.py
+++ src/app.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-print('hello world!')
+import json
+import os
+import requests
+
+r = requests.post(
+    'https://api.rainforestcloud.com/rest/auth/accesskey',
+    json.dumps({
+      "login": os.environ['RAINFOREST_USERNAME'],
+      "password": os.environ['RAINFOREST_PASSWORD'],
+    }))
+
+print(r.json())
